28c5e977e67ebbdf9dcc3ed202a34462c85de753
Victor Carpenter <victorcarpenter123@gmail.com>
2016-10-30 14:25:24 -0400

    inital commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,62 @@
+const $ = require('jquery')
 const Backbone = require('Backbone')
 
-console.log(Backbone)
-document.querySelector('#app-container').innerHTML = `<h1>YOLO</h1>`
+
+var appHolder = document.querySelector('#app-container')
+console.log(appHolder)
+
+var etsyModel = Backbone.Model.extend({
+
+})
+
+var etsyCollection = Backbone.Collection.extend({
+   model: etsyModel,
+   url: '',
+   parse: function(rawJSON){
+      console.log(rawJSON.results)
+      return rawJSON.results
+
+   },
+   sync : function(method, collection, options) {
+
+    options.dataType = "jsonp";
+    return Backbone.sync(method, collection, options);
+  },
+
+   initialize: function(qryStr){
+      this.url = 	"https://openapi.etsy.com/v2/listings/active.js?api_key=a4rxc0l54zj63ku2c0of02ic"
+   }
+})
+
+const appRouter = Backbone.Router.extend({
+   routes: {
+      "moreInfo/:id" : "showMorePage",
+      "" : "showHomePage"
+   },
+
+   showHomePage: function(){
+      var coll = new etsyCollection()
+      coll.fetch().then(function(){
+         console.log(coll.models)
+
+      })
+   },
+
+   initialize: function(){
+      Backbone.history.start()
+   }
+})
+
+
+const app = new appRouter();
+
+function getData(data) {
+      if (data.ok) {
+            // do something with the data here
+        } else {
+            alert(data.error);
+      }
+}
+
+
+console.log('hey')
